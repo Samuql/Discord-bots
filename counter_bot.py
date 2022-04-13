71dbd99ccebe281d9bb759ff8ad90e956acb6453
Samuql <103607709+Samuql@users.noreply.github.com>
2022-04-13 23:51:06 +0200

    Add files via upload

---
+++ counter_bot.py
@@ -0,0 +1,48 @@
+import discord
+import json
+
+keywords = ["höhö", "nenene", "do hond", "<:]", "niemals", ": >", "altaaa"]
+
+
+class MyClient(discord.Client):
+
+    # bot login
+    async def on_ready(self):
+        print("Bot online")
+
+    def get_score(self, member: discord.Member):
+        with open("users.json") as users:
+            dictlist = json.load(users)
+
+        id = str(member.id)
+        for k in dictlist:
+            if k == id:
+                return dictlist[k]  # return val corresponding to id
+
+    def add_score(self, member: discord.Member, amount: float):
+        with open("users.json") as jsonFile:
+            dictlist = json.load(jsonFile)
+
+        id = str(member.id)
+        if id not in dictlist:
+            dictlist[id] = amount  # create new dict entry for user
+        else:
+            dictlist[id] += amount
+        new = dictlist
+
+        with open("users.json", "w+") as dictlist:
+            json.dump(new, dictlist, sort_keys=True, indent=4)  # save the changes
+
+    # When a message is sent
+    async def on_message(self, message):
+        if message.author != client.user:
+            author = message.author
+            if message.content in keywords:
+                self.add_score(author, 0.01)
+                await message.channel.send(
+                    message.content + " ??? " + author.mention + " Muss 1ct bezahlen. Damit hat " + author.name
+                    + " nun Schulden von insgesamt " + str(round(self.get_score(author), 3)) + "€!")
+
+
+client = MyClient()
+client.run("TOKEN")  # that's the client token. don't share it
